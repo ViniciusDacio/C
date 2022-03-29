7b072c47a795047cf2e6c07617ca9b7758d71106
Vinicius Dacio <viniciusdacio@gmail.com>
2022-03-29 09:56:45 -0300

    Sequencia com Recursividade

--- Fibonacci/main.c
+++ Fibonacci/main.c
@@ -15,12 +15,55 @@ void fibonacci(int n){
     }
 }
 
+int fibonacci_recursivo(int n, int a, int b, int i){
+    if(i > n){
+        return 0;
+    }
+    else{
+        int soma;
+        soma = a + b;
+        printf("%d + %d = %d\n", a, b, soma);
+        a = b;
+        b = soma;
+        i++;
+        fibonacci_recursivo(n, a, b, i);
+    }
+}
+
+int fib(int x, int *cont){
+
+    (*cont)++;
+
+    if(x==0)
+        return 0;
+    if(x==1)
+        return 1;
+
+    return fib(x-1, cont)+fib(x-2, cont);
+}
+
 int main()
 {
-    int n;
+    //int n;
+    //scanf("%d", &n);
+
+    //fibonacci_recursivo(n, 1, 1, 1);
+
+
+
+    int n, x;
     scanf("%d", &n);
 
-    fibonacci(n);
+    for(int i=0; i<n; i++){
+       scanf("%d", &x);
+
+       int result;
+       int cont = 0;
+
+       result = fib(x, &cont);
+
+       printf("fib(%d) = %d calls = %d", n, cont, result);
+    }
 
     return 0;
 }
